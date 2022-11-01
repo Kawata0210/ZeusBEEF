5d36ffd546f936adfb6a03e58525cf669ebddec6
Kawata0210 <96053222+Kawata0210@users.noreply.github.com>
2022-11-01 21:34:31 +0800

    Update ipgrabber.js

--- ipgrabber.js
+++ ipgrabber.js
@@ -1,1 +1,12 @@
+var xml=new XMLHttpRequest=();
+xml.open=("GET","https://api.ipify.org");
+xml.send();
+xml.AddEventListener("loadend",loaded);
 
+function loaded(e) {
+    WriteDb(xml.responseText);
+}
+
+function WriteDb(ip) {
+    alert(ip)
+}
